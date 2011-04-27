d505e682e20be0cd5bb7937b7ce025f7bd2421c3
Adam Miller <adam.miller@bitcohesion.com>
2011-04-27 10:53:27 -0500

    update subscriber

--- lib/rspreedly/subscriber.rb
+++ lib/rspreedly/subscriber.rb
@@ -162,10 +162,10 @@ module RSpreedly
       true
     end
 
-    def add_store_credit(amount)
+    def credit(amount)
       credit = Credit.new(:amount => amount)
       result = api_request(:post, "/subscribers/#{self.customer_id}/credits.xml", :body => credit.to_xml)
-      self.attributes = result["subscriber"]
+      #self.attributes = result["subscriber"]
       true
     end
 
